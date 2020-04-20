266d01d0700f27bb08555152b9477a6ea619d0ec
NutPea <p.nuss96@outlook.de>
2020-04-21 01:51:14 +0200

    DialogSystem

---
+++ LundumDare46/Assets/Scripts/PlayerUI/DialogeManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogeManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+    public TextMeshProUGUI textDialoge;
+
+    public GameObject endOf;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialoge(Dialoge dialoge){
+        sentences.Clear();
+        foreach(string sentence in dialoge.sentences){
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence(){
+        if(sentences.Count == 0){
+            EndDialoge();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        textDialoge.text = sentence;
+        
+    }
+
+    private void EndDialoge()
+    {
+        endOf.SetActive(false);
+    }
+}
---
+++ LundumDare46/Assets/Scripts/PlayerUI/DialogeTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogeTrigger : MonoBehaviour
+{
+
+public Dialoge dialoge;
+
+
+private void Start() {
+    FindObjectOfType<DialogeManager>().StartDialoge(dialoge);
+}
+  
+}
